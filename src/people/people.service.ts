26e0fe324dd272140394b69cd2b7b8971bbe90e0
mtw-programmer <visnia58@gmail.com>
2024-12-11 17:56:09 +0100

    chore: add ts types to res

--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { SwapiUtils } from 'src/common/utils/swapi.utils';
+import { PeopleResponseDto, PersonResponseDto } from './dto/people-response.dto';
 
 @Injectable()
 export class PeopleService {
@@ -7,18 +8,18 @@ export class PeopleService {
 
     private readonly toFetch = ['homeworld', 'films', 'species', 'vehicles', 'starships'];
 
-    async getAllPeople(deep: boolean, filters: Record<string, any>): Promise<any> {
+    async getAllPeople(deep: boolean, filters: Record<string, any>): Promise<PeopleResponseDto> {
         try {
-            return await this.swapiUtils.fetchAllData('people', deep ? this.toFetch : [], filters) as any;
+            return await this.swapiUtils.fetchAllData('people', deep ? this.toFetch : [], filters) as PeopleResponseDto;
         } catch (error) {
             console.error(`getAllPeople: ${error}`);
             throw new InternalServerErrorException('Something went wrong! Please, try again later.');
         }
     }
 
-    async getPersonById(id: number, deep: boolean): Promise<any> {
+    async getPersonById(id: number, deep: boolean): Promise<PersonResponseDto> {
         try {
-            return await this.swapiUtils.fetchOne(`people/${id}`, deep ? this.toFetch : []) as any;
+            return await this.swapiUtils.fetchOne(`people/${id}`, deep ? this.toFetch : []) as PersonResponseDto;
         } catch (error) {
             if (error instanceof NotFoundException) {
                 throw error;
--- src/starships/starships.service.ts
+++ src/starships/starships.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { SwapiUtils } from 'src/common/utils/swapi.utils';
+import { StarshipResponseDto, StarshipsResponseDto } from './dto/startships-response.dto';
 
 @Injectable()
 export class StarshipsService {
@@ -7,18 +8,18 @@ export class StarshipsService {
 
     private readonly toFetch = ['films', 'pilots'];
 
-    async getAllStarships(deep: boolean, filters: Record<string, any>): Promise<any> {
+    async getAllStarships(deep: boolean, filters: Record<string, any>): Promise<StarshipsResponseDto> {
         try {
-            return await this.swapiUtils.fetchAllData('starships', deep ? this.toFetch : [], filters) as any;
+            return await this.swapiUtils.fetchAllData('starships', deep ? this.toFetch : [], filters) as StarshipsResponseDto;
         } catch (error) {
             console.error(`getAllStarships: ${error}`);
             throw new InternalServerErrorException('Something went wrong! Please, try again later.');
         }
     }
 
-    async getStarshipById(id: number, deep: boolean): Promise<any> {
+    async getStarshipById(id: number, deep: boolean): Promise<StarshipResponseDto> {
         try {
-            return await this.swapiUtils.fetchOne(`starships/${id}`, deep ? this.toFetch : []) as any;
+            return await this.swapiUtils.fetchOne(`starships/${id}`, deep ? this.toFetch : []) as StarshipResponseDto;
         } catch (error) {
             if (error instanceof NotFoundException) {
                 throw error;
