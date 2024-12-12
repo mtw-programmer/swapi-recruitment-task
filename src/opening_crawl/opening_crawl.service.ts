9eba294b9ad615b93a0a97b30360c4a86a77b1f3
mtw-programmer <visnia58@gmail.com>
2024-12-12 14:52:56 +0100

    feat(opening_crawl): add word pairs

--- src/opening_crawl/opening_crawl.service.ts
+++ src/opening_crawl/opening_crawl.service.ts
@@ -16,6 +16,17 @@ export class OpeningCrawlService {
 
         openingCrawls = openingCrawls.replace(/[.,-?!&;:]/g, '');
         const words = openingCrawls.split(/\s+|\n|\r|\r\n/).filter((word: string) => word !== '');
-        return words;
+
+        if (!words.length) return [];
+
+        const uniquePars = {};
+        
+        for (let i = 1; i <= words.length - 1; i++) {
+            const pair = `${words[i-1]} ${words[i]}`;
+            uniquePars[pair] = (uniquePars[pair] || 0) + 1;
+        }
+
+        const uniqueWordPairs = Object.entries(uniquePars).map(([pair, count]) => [pair, count]);
+        return { uniqueWordPairs };
     }
 }
